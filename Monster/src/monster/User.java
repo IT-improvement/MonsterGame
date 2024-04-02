26b08794695c735908518bb2759259cfb037848a
seungjae <juaje03@naver.com>
2024-04-02 17:04:17 +0900

    Add: name, power, hp, maxHp, mp, MaxMp variable and each of getter, setter Method in User Abstract Class

---
+++ Monster/src/monster/User.java
@@ -0,0 +1,42 @@
+package monster;
+
+public abstract class User {
+	private String name;
+	private int power;
+	private int hp;
+	private int MAX_HP;
+	private int mp;
+	private int MAX_MP;
+
+	public User(String name, int power, int hp, int mp) {
+		this.name = name;
+		this.power = power;
+		MAX_HP = this.hp = hp;
+		MAX_MP = this.mp = mp;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getMp() {
+		return mp;
+	}
+
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+
+}
