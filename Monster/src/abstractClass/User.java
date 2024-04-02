d57079b7fd387c51bcb5fe02fcda73c14bc238c6
seungjae <juaje03@naver.com>
2024-04-02 17:18:45 +0900

    Add: isDead Variable Modify: setHp Method in User Abstract Class

--- Monster/src/abstractClass/User.java
+++ Monster/src/abstractClass/User.java
@@ -7,6 +7,7 @@ public abstract class User {
 	private int MAX_HP;
 	private int mp;
 	private int MAX_MP;
+	private boolean isDead;
 
 	public User(String name, int power, int hp, int mp) {
 		this.name = name;
@@ -28,7 +29,13 @@ public abstract class User {
 	}
 
 	public void setHp(int hp) {
-		this.hp = hp;
+		this.hp += hp;
+		if (this.hp == 0) {
+			System.out.println("사망");
+			isDead = true;
+		}
+		if (this.hp > MAX_HP)
+			this.hp = MAX_HP;
 	}
 
 	public int getMp() {
