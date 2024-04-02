7ec3289af4d79df61add42cde72185d8551ad7a2
seungjae <juaje03@naver.com>
2024-04-02 17:21:09 +0900

    Add: implements Skillable

--- Monster/src/unit/Warrior.java
+++ Monster/src/unit/Warrior.java
@@ -1,11 +1,17 @@
 package unit;
 
 import abstractClass.User;
+import interfaces.Skillable;
 
-public class Warrior extends User {
+public class Warrior extends User implements Skillable {
 
 	public Warrior(String name, int power, int hp, int mp) {
 		super("전사", 40, 200, 30);
+	}
+
+	@Override
+	public int skill() {
+		return 0;
 	}
 
 }
--- Monster/src/unit/Wizard.java
+++ Monster/src/unit/Wizard.java
@@ -1,11 +1,17 @@
 package unit;
 
 import abstractClass.User;
+import interfaces.Skillable;
 
-public class Wizard extends User {
+public class Wizard extends User implements Skillable{
 
 	public Wizard(String name, int power, int hp, int mp) {
 		super("마법사", 50, 100, 100);
+	}
+
+	@Override
+	public int skill() {
+		return 0;
 	}
 
 }
