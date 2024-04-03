ece8c6c268c94cff0d2532992fbd38258a471e69
seungjae <juaje03@naver.com>
2024-04-03 14:24:58 +0900

    Add: fight Monster & User Logic in Game Class

--- Monster/src/main/Game.java
+++ Monster/src/main/Game.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 import abstractClass.Map;
 import abstractClass.Monster;
 import abstractClass.User;
+import interfaces.Skillable;
 import map.Map1;
+import monster.Golem;
 import unit.Begginer;
 
 public class Game {
@@ -23,10 +25,31 @@ public class Game {
 
 	public static User user;
 	private Map map;
+	private Monster monster;
 
 	public Game() {
 		user = new Begginer();
 		map = new Map1();
+		monster = null;
+	}
+
+	// input String
+	private String inputString(String message) {
+		System.out.print(message + ": ");
+		return scan.next();
+	}
+
+	// input Num
+	private int inputNum(String message) {
+		int num = -1;
+		try {
+			System.out.print(message + ": ");
+			String temp = scan.next();
+			num = Integer.parseInt(temp);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return num;
 	}
 
 	/* print */
@@ -36,9 +59,53 @@ public class Game {
 		System.out.println("2) 회원가입" + ANSI_RESET);
 	}
 
-	private String inputString(String message) {
-		System.out.print(message + ": ");
-		return scan.next();
+	private void printMonster(Monster monster) {
+		System.out.println(ANSI_BOLD + ANSI_RED + "몬스터 등장!!");
+		System.out.println(monster);
+	}
+
+	private void printBegginer() {
+		System.out.println("1) 평차치기");
+		System.out.println("*) 도망가기");
+	}
+
+	private void printFristJob() {
+		System.out.println("1) 평타치기");
+		System.out.println("3) 스킬쓰기");
+		System.out.println("*) 도망가기");
+
+	}
+
+	private void begginer(int sel) {
+		switch (sel) {
+		case 1:
+			bassicAttack();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void fristJob(int sel) {
+		switch (sel) {
+		case 1:
+			bassicAttack();
+			break;
+		case 2:
+			firstSkill();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void bassicAttack() {
+		monster.setHp(-user.getPower());
+	}
+
+	private void firstSkill() {
+		int damage = user.skill();
+		monster.setHp(-damage);
 	}
 
 	public void run() {
@@ -47,10 +114,11 @@ public class Game {
 			System.out.println(map);
 			String dir = inputString(ANSI_RESET + "입력");
 			move(dir);
-			if (isFight() == null) {
+			monster = isFight();
+			if (monster == null) {
 				continue;
 			}
-			fight();
+			fight(monster);
 		}
 	}
 
@@ -81,7 +149,29 @@ public class Game {
 		return null;
 	}
 
-	private void fight() {
+	private void fight(Monster monster) {
+		printMonster(monster);
+		battle(monster);
+	}
 
+	private void battle(Monster monster) {
+		while (!monster.isDead() && !user.isDead()) {
+			userAttack();
+			monsterAttack();
+		}
+	}
+
+	private void userAttack() {
+		if (user instanceof Skillable) {
+			printFristJob();
+			fristJob(inputNum("메뉴입력"));
+		} else {
+			printBegginer();
+			begginer(inputNum("메뉴입력"));
+		}
+	}
+
+	private void monsterAttack() {
+		user.setHp(-monster.getPower());
 	}
 }
