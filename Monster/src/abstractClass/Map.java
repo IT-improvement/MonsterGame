7fb39a97c466a5021ce75dafeda7805891475ba0
seungjae <juaje03@naver.com>
2024-04-03 11:50:13 +0900

    Add: toString Method in Map Class

--- Monster/src/abstractClass/Map.java
+++ Monster/src/abstractClass/Map.java
@@ -2,6 +2,8 @@ package abstractClass;
 
 import java.util.ArrayList;
 
+import main.Game;
+
 public abstract class Map {
 	private int height;
 	private int width;
@@ -29,6 +31,37 @@ public abstract class Map {
 	public void setWidth(int width) {
 		this.width = width;
 	}
+
 	public abstract void setMonster();
 
+	@Override
+	public String toString() {
+		String result = "";
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				boolean isCheck = false;
+				for (Monster monster : monsterList) {
+					if (monster.getY() == i && monster.getX() == j) {
+						if (i == Game.user.getY() && j == Game.user.getX()) {
+							isCheck = true;
+							result += Game.ANSI_RED + "🔫‍ ";
+						} else {
+							isCheck = true;
+							result += Game.ANSI_GREEN + "🌳 ";
+						}
+					}
+				}
+				if (!isCheck) {
+					if (i == Game.user.getY() && j == Game.user.getX()) {
+						result += Game.ANSI_RESET + "🧍‍♂️ ";
+					} else {
+						result += Game.ANSI_BROWN + "🟫 ";
+					}
+				}
+			}
+			result += "\n";
+		}
+
+		return result;
+	}
 }
