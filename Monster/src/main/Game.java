de2971689529bc4742710fca8db0e595de70f47c
seungjae <juaje03@naver.com>
2024-04-03 17:29:55 +0900

    Update Game version1.1

--- Monster/src/main/Game.java
+++ Monster/src/main/Game.java
@@ -13,13 +13,27 @@ import unit.Begginer;
 
 public class Game {
 	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BOLD = "\u001B[1m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_YELLOW = "\u001B[33m";
-	public static final String ANSI_BOLD = "\u001B[1m";
 	public static final String ANSI_GREEN = "\u001B[32m";
 	public static final String ANSI_RED = "\u001B[31m";
 	public static final String ANSI_BROWN = "\u001B[33m";
 	public static final String ANSI_GRAY = "\u001B[90m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_BLACK = "\u001B[30m"; // 검정색
+	public static final String ANSI_BLUE = "\u001B[34m"; // 파란색
+	public static final String ANSI_WHITE = "\u001B[37m"; // 흰색
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m"; // 검정색 배경
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m"; // 빨간색 배경
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m"; // 녹색 배경
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m"; // 노란색 배경
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m"; // 파란색 배경
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m"; // 자주색 배경
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m"; // 시안색 배경
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m"; // 흰색 배경
+	public static final String ANSI_PURPLE_BACKGROUND_LIGHT = "\u001B[45;1m"; // 자주색 배경
 
 	private Scanner scan = new Scanner(System.in);
 
@@ -58,8 +72,8 @@ public class Game {
 
 	public void run() {
 		while (isRun) {
-			printStatusUser();
-			printStart();
+			// printStart();
+			System.out.println(map.getName() + ANSI_RESET);
 			System.out.println(map);
 			String dir = inputString(ANSI_RESET + "입력");
 			move(dir);
@@ -79,40 +93,33 @@ public class Game {
 	}
 
 	private void printMonster() {
-		System.out.println(ANSI_BOLD + ANSI_RED + "몬스터 등장!!");
 		System.out.println(monster);
 	}
 
 	private void printBegginer() {
-		System.out.println("1) 평타치기");
-		System.out.println("*) 도망가기");
+		System.out.println("============");
+		System.out.println("| 1) 평타치기 |");
+		System.out.println("| *) 도망가기 |");
+		System.out.println("============");
 	}
 
 	private void printFristJob() {
-		System.out.println("1) 평타치기");
-		System.out.println("2) 스킬쓰기");
-		System.out.println("*) 도망가기");
+		System.out.println("============");
+		System.out.println("| 1) 평타치기 |");
+		System.out.println("| 2) 스킬쓰기 |");
+		System.out.println("| *) 도망가기 |");
+		System.out.println("============");
 
 	}
 
-	private void printUserAttack() {
-		printStatusUser();
-		System.out.println(Game.ANSI_RED);
-		printStatusMonster();
-		System.out.println(Game.ANSI_RESET);
-	}
 
 	private void printMonsterAttack() {
 		System.out.println(monster.getMessage());
-		System.out.println(Game.ANSI_RED);
-		printStatusUser();
-		System.out.println(Game.ANSI_RESET);
-		printStatusMonster();
+		String result = "";
+		result += ANSI_RED + user.getName() + ": [" + user.getHp() + "|" + user.getMAX_HP() + "]" + Game.ANSI_RESET;
+		System.out.println(result);
 	}
 
-	private void printStatusUser() {
-		System.out.println(user.getName() + ": [" + user.getHp() + "|" + user.getMAX_HP() + "]");
-	}
 
 	private void printStatusMonster() {
 		System.out.println(monster.getName() + ": [" + monster.getHp() + "|" + monster.getMAX_HP() + "]");
@@ -145,10 +152,12 @@ public class Game {
 	}
 
 	private void bassicAttack() {
+		System.out.println("평타!!");
 		monster.setHp(-user.getPower());
 	}
 
 	private void firstSkill() {
+		System.out.println("스킬~");
 		int damage = user.skill();
 		monster.setHp(-damage);
 	}
@@ -183,27 +192,32 @@ public class Game {
 	}
 
 	private void fight() {
-		printMonster();
+		System.out.println(ANSI_BOLD + ANSI_RED + "몬스터 등장!!");
 		battle();
 	}
 
 	private void battle() {
 		while (true) {
+			printMonster();
+			printStatusMonster();
 			userAttack();
 			if (monster.isDead()) {
 				deadMonster();
 			}
 			if (!isFight)
 				break;
-
-			printUserAttack();
+			try {
+				Thread.sleep(2000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 			monsterAttack();
+			printMonsterAttack();
 			if (user.isDead()) {
 				deadUser();
 			}
 			if (!isFight)
 				break;
-			printMonsterAttack();
 		}
 	}
 
