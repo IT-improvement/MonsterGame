4f031971569a084c87a39108d291503fc1e43d42
seungjae <juaje03@naver.com>
2024-04-04 18:13:30 +0900

    Add: user,itemList HashMap Variable in ItemList

--- Monster/src/item/ItemList.java
+++ Monster/src/item/ItemList.java
@@ -1,11 +1,26 @@
 package item;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import abstractClass.Item;
+import abstractClass.Job;
 
 public class ItemList {
 	private ArrayList<Item> itemList;
+	private static HashMap<Job, ArrayList<Item>> userItemList;
 
 	public ItemList() {
 		itemList = new ArrayList<>();
+		userItemList = new HashMap<>();
+	}
+
+	private void setItemList() {
+		itemList.add(addWeapon("방망이", 1000, 10));
+	}
+
+	private Item addWeapon(String name, int price, int power) {
+		Item item = new Weapon(name, price, power);
+		return item;
 	}
 }
