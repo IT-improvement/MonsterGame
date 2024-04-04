356ff20503a52f2ac06c510664dce563a19dbb50
seungjae <juaje03@naver.com>
2024-04-04 18:35:14 +0900

    Create: findId, findNickName Method in UserManager Class

--- Monster/src/user/UserManager.java
+++ Monster/src/user/UserManager.java
@@ -1,5 +1,31 @@
 package user;
 
-public class UserManager {
+import java.util.ArrayList;
 
+public class UserManager {
+	private ArrayList<User> userList;
+
+	private UserManager() {
+		userList = new ArrayList<>();
+	}
+
+	private static UserManager instance = new UserManager();
+
+	public static UserManager getInstance() {
+		return instance;
+	}
+
+	public String findId(String id) {
+		for (User user : userList)
+			if (user.getId().equals(id))
+				return user.getId();
+		return null;
+	}
+
+	public String findNickName(String nickName) {
+		for (User user : userList)
+			if (user.getNickName().equals(nickName))
+				return user.getNickName();
+		return null;
+	}
 }
