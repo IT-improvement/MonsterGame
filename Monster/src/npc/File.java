1931f2c79fd2e2152e6ac96c36e389d373265ce9
seungjae <juaje03@naver.com>
2024-04-05 11:27:14 +0900

    Add: toString, guideMessage Method in File Class

--- Monster/src/npc/File.java
+++ Monster/src/npc/File.java
@@ -1,6 +1,7 @@
 package npc;
 
 import abstractClass.Npc;
+import main.FontStyle;
 
 public class File extends Npc {
 
@@ -10,6 +11,25 @@ public class File extends Npc {
 
 	@Override
 	public void printGuideMessage() {
-		
+		String line = FontStyle.ANSI_BLUE + "=========================";
+		String side = FontStyle.ANSI_BLUE + "|";
+		String result = "";
+		result += line + "\n";
+		result += side + FontStyle.ANSI_CYAN + "\t\t1. 저장\t\t" + side + "\n";
+		result += side + FontStyle.ANSI_CYAN + "\t\t2. 나가기\t\t" + side + "\n";
+		result += line+"\n";
+		System.out.println(result);
+	}
+	
+	@Override
+	public String toString() {
+		String result = "";
+		String welcomeMessage = FontStyle.ANSI_CYAN+" 파일매니저에 오신걸 환영합니다 ";
+		int messageLength = welcomeMessage.length();
+
+		String line2 = FontStyle.ANSI_BLUE +" " + "\u25cf".repeat(messageLength+2);
+
+		result = "\n" + line2 + "\n" + "\u25cf " + welcomeMessage + FontStyle.ANSI_BLUE +" \u25cf\n" + line2;
+		return result;
 	}
 }
