d31d2064356d64eb95d34aa66e2581f914ddcf11
seungjae <juaje03@naver.com>
2024-04-05 10:26:47 +0900

    Add: instance Method in FileManager Class

--- Monster/src/file/FileManager.java
+++ Monster/src/file/FileManager.java
@@ -1,5 +1,13 @@
 package file;
 
 public class FileManager {
-	
+	private FileManager() {
+
+	}
+
+	private static FileManager instance = new FileManager();
+
+	public static FileManager getInstance() {
+		return instance;
+	}
 }
