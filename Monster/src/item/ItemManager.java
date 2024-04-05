e322b1c7b3d319c3db40256e76eb40ee6fda65d5
seungjae <juaje03@naver.com>
2024-04-05 16:16:24 +0900

    Add: addItem Method in ItemManager Class

--- Monster/src/item/ItemManager.java
+++ Monster/src/item/ItemManager.java
@@ -4,19 +4,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import abstractClass.Item;
-import abstractClass.Job;
+import main.Game;
+import user.User;
 
 public class ItemManager {
-	private static HashMap<Job, ArrayList<Item>> userItemList;
+	private static HashMap<User, ArrayList<Item>> userItemList;
+
+	private ItemList list;
 
 	private ItemManager() {
 		userItemList = new HashMap<>();
+		list = new ItemList();
 	}
 
 	private static ItemManager instance = new ItemManager();
 
 	public static ItemManager getInstance() {
 		return instance;
+	}
+
+	public void addItem(String itemName) {
+		User user = Game.user;
+		Item target = checkItem(itemName);
+		ArrayList<Item> targetItem = addItemList(user, target);
+		userItemList.put(user, targetItem);
+	}
+
+	private Item checkItem(String itemName) {
+		ArrayList<Item> itemList = list.getItemList();
+		Item target = null;
+		for (Item item : itemList)
+			if (item.getName().equals(itemName))
+				target = item;
+		return target;
+	}
+
+	private ArrayList<Item> addItemList(User user, Item target) {
+		ArrayList<Item> targetItem = userItemList.get(user);
+		if (targetItem == null) {
+			targetItem = new ArrayList<>();
+		}
+		targetItem.add(target);
+		return targetItem;
 	}
 
 }
