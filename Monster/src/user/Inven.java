c755287ec27ac50ce620ae17bd05435cbdd4e9e5
puppy <puppy@221.145.17.22>
2024-04-09 00:14:13 +0900

    Add: Output the currently worn equipment in Inven Class

--- Monster/src/user/Inven.java
+++ Monster/src/user/Inven.java
@@ -46,6 +46,18 @@ public class Inven {
 		}
 
 		result += FontStyle.ANSI_RESET + "\n===========\n";
+		result += "무기: ";
+		if (Game.job.getWeapon() == null)
+			result += "없음\n";
+		else
+			result += Game.job.getWeapon().getName() + "\n";
+		result += "방어구: ";
+		if (Game.job.getDefense() == null)
+			result += "없음\n";
+		else
+			result += Game.job.getDefense().getName() + "\n";
+		result += "반지: ";
+		result += "없음\n";
 		return result;
 	}
 }
